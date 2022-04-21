e89d0249b62c81ba40d4a75e57b694cf8033d5ce
Zhuoran Li <zhuoran3@asu.edu>
2022-04-21 01:11:14 -0700

    update parse tree and gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -6,23 +7,31 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class Main extends JFrame {
     private static final int DEFAULT_WINDOW_HEIGHT = 500;
     private static final int DEFAULT_WINDOW_WIDTH = 800;
-
+    JTextArea input;
+    JTextArea output;
     public Main(){
         super("Mocha");
         JPanel Mocha = new JPanel();
-        JTextArea editor = new JTextArea();
+        input = new JTextArea(20,65);
+        JScrollPane inputPane = new JScrollPane(input);
+        output = new JTextArea(5,50);
+        JScrollPane outputPane = new JScrollPane(output);
+        input.setLineWrap(true);
+        output.setLineWrap(true);
         JButton Run = new JButton("run");
         Run.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                lexer(editor.getText());
+                lexer(input.getText());
             }
         });
-        Mocha.add(editor);
+        Mocha.add(inputPane);
+        Mocha.add(outputPane);
         Mocha.add(Run);
         add(Mocha);
     }
@@ -34,11 +43,12 @@ public class Main extends JFrame {
         mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    public void lexer(String MochaContent){
-        MochaLexer mochaLexer = new MochaLexer(CharStreams.fromString(MochaContent));
+    public void lexer(String mochaContent){
+        MochaLexer mochaLexer = new MochaLexer(CharStreams.fromString(mochaContent));
         CommonTokenStream tokens = new CommonTokenStream(mochaLexer);
         MochaParser parser = new MochaParser(tokens);
         ParseTree tree = parser.program();
+        output.setText(tree.toStringTree(parser));
         ParseTreeWalker walker = new ParseTreeWalker();
     }
 }
